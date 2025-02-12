9fef1b7b1749e8d76a43e45b7a5d153712bf0967
unknown <xristosdurros@hotmail.com>
2025-02-12 13:40:48 +0200

    Removal of unnecessary lines

--- src/main/java/com/cdurro/controller/SeatController.java
+++ src/main/java/com/cdurro/controller/SeatController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +17,6 @@ import com.cdurro.model.Seat;
 import com.cdurro.service.SeatService;
 
 @RestController
-@CrossOrigin(origins = { "http://localhost:5173" })
 public class SeatController {
 	
 	@Autowired
@@ -77,7 +75,7 @@ public class SeatController {
 	
 	@PutMapping("/seats/update")
 	public ResponseEntity<Seat> updateSeat(@RequestBody Seat seat) {
-		System.out.println("update called!");
+
 		return seatService.updateSeat(seat);
 	}
 	
@@ -87,7 +85,6 @@ public class SeatController {
 		return seatService.updateSeats(body);
 	}
 	
-
 	@PutMapping("/seats/availability/reset/{id}")
 	public Seat resetSeatAvailability(@PathVariable Long id) {
 		
